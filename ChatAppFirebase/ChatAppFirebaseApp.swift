7d9aeb0aafc77a6b1e573ed5d3d021b77298e37c
Berkay-Disli <berkaydisli.work@gmail.com>
2022-07-01 15:48:28 +0300

    Initial Commit

---
+++ ChatAppFirebase/ChatAppFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppFirebaseApp.swift
+//  ChatAppFirebase
+//
+//  Created by Berkay Disli on 1.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
